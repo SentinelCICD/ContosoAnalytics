c7b9348979b2a6b269b8e1036883d1c90dd0d6ab
ertoleda <ertolead@microsoft.com>
2021-04-19 15:01:05 -0700

    deploy AR 5

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,14 +1,8 @@
-Write-Output "path: $Env:path"
 Write-Output "rg name: $Env:resourceGroupName"
 Write-Output "ws name: $Env:workspaceName"
-$loc = Get-Location
-Write-Output $loc
+Write-Output "Starting Deployment for Files in path: $Env:directory"
 
-$BasePath = "${Env:path}\Detections:"
-
-Write-Output "Starting Deployment for Files in path: $BasePath"
-
-Get-ChildItem $BasePath -Filter *.json |
+Get-ChildItem $Env:directory -Filter *.json |
 ForEach-Object {
     $FullPathToTemplateFile = $_.FullName
     New-AzManagementGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $FullPathToTemplateFile -logAnalyticsWorkspaceName $Env:workspaceName
