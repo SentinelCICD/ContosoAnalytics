eedea57ffc50aae582db5961a3eaf6e8f78879e6
ertoleda <ertolead@microsoft.com>
2021-04-20 11:42:50 -0700

    test secret 1

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,5 +1,11 @@
 Write-Output "rg name: $Env:resourceGroupName"
 Write-Output "ws name: $Env:workspaceName"
+Write-Output "creds: $Env:creds"
+
+# Write-Output "Attempting Sign In"
+# Connect-AzAccount -ServicePrincipal -Tenant "$Env:tenantId" -Credential \
+# (New-Object System.Management.Automation.PSCredential('${servicePrincipalId}'))
+
 Write-Output "Starting Deployment for Files in path: $Env:directory"
 
 Get-ChildItem $Env:directory -Filter *.json |
