24571e7eec7fb213ccdf109c6ac53553808d5dbd
ertoleda <ertolead@microsoft.com>
2021-04-20 12:42:27 -0700

    test signin 1

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,16 +1,32 @@
 Write-Output "rg name: $Env:resourceGroupName"
 Write-Output "ws name: $Env:workspaceName"
-Write-Output "creds: $Env:creds"
-$c = $Env:creds | ConvertFrom-Json
-Write-Output "clientId: " $c.clientId
-
-# Write-Output "Attempting Sign In"
-# Connect-AzAccount -ServicePrincipal -Tenant "$Env:tenantId" -Credential \
-# (New-Object System.Management.Automation.PSCredential('${servicePrincipalId}'))
 
 Write-Output "Starting Deployment for Files in path: $Env:directory"
+
+AttemptSignIn
 
 Get-ChildItem $Env:directory -Filter *.json |
 ForEach-Object {
     New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $_.FullName -logAnalyticsWorkspaceName $Env:workspaceName
+}
+
+function AttemptSignIn {
+    Write-Output "Attempting Sign In"
+    $RawCreds = $Env:creds | ConvertFrom-Json
+
+    Clear-AzContext -Scope Process;
+    Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue;
+    
+    Add-AzEnvironment `
+        -Name $Env:azureCloud `
+        -ARMEndpoint $RawCreds.resourceManagerEndpointUrl `
+        -ActiveDirectoryEndpoint $RawCreds.activeDirectoryEndpointUrl `
+        -GraphResourceId $RawCreds.activeDirectoryGraphResourceId `
+        | out-null;
+
+    $servicePrincipalKey = ConvertTo-SecureString $RawCreds.clientSecret.replace("'", "''") -AsPlainText -Force
+    $psCredential = New-Object System.Management.Automation.PSCredential($RawCreds.clientId, $servicePrincipalKey)
+
+    Connect-AzAccount -ServicePrincipal -Tenant $Env:tenantId -Credential $psCredential
+
 }
