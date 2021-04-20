7515a8a02fc3486dbf1f3d048e2a18f41f89ff9c
ertoleda <ertolead@microsoft.com>
2021-04-20 12:53:30 -0700

    test signin 2

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,18 +1,6 @@
-Write-Output "rg name: $Env:resourceGroupName"
-Write-Output "ws name: $Env:workspaceName"
-
-Write-Output "Starting Deployment for Files in path: $Env:directory"
-
-AttemptSignIn
-
-Get-ChildItem $Env:directory -Filter *.json |
-ForEach-Object {
-    New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $_.FullName -logAnalyticsWorkspaceName $Env:workspaceName
-}
-
 function AttemptSignIn {
     Write-Output "Attempting Sign In"
-    $RawCreds = $Env:creds | ConvertFrom-Json
+    $RawCreds = '{"clientId: "12", "clientSecret": "34"}' # $Env:creds | ConvertFrom-Json
 
     Clear-AzContext -Scope Process;
     Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue;
@@ -27,6 +15,16 @@ function AttemptSignIn {
     $servicePrincipalKey = ConvertTo-SecureString $RawCreds.clientSecret.replace("'", "''") -AsPlainText -Force
     $psCredential = New-Object System.Management.Automation.PSCredential($RawCreds.clientId, $servicePrincipalKey)
 
-    Connect-AzAccount -ServicePrincipal -Tenant $Env:tenantId -Credential $psCredential
+    Connect-AzAccount -ServicePrincipal -Tenant $Env:tenantId -Credential $psCredential -Environment $Env:azureCloud
+}
 
+if (-NOT $Env:azureCloud -eq "Prod") {
+    AttemptSignIn
+}
+
+Write-Output "Starting Deployment for Files in path: $Env:directory"
+
+Get-ChildItem $Env:directory -Filter *.json |
+ForEach-Object {
+    New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $_.FullName -logAnalyticsWorkspaceName $Env:workspaceName
 }
