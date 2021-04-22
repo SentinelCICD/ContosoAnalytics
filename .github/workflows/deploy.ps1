db65ddea9972736fd97c42b7cdafd366e41e3d14
ertoleda <ertolead@microsoft.com>
2021-04-22 09:41:44 -0700

    test signin 1

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -6,17 +6,16 @@ function AttemptSignIn {
     Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue;
     
     Add-AzEnvironment `
-        -Name $Env:azureCloud `
-        -ARMEndpoint $RawCreds.resourceManagerEndpointUrl `
+        -Name "Dogfood" `
+        -ActiveDirectoryServiceEndpointResourceId "https://management.core.windows.net/" `
         -ActiveDirectoryEndpoint $RawCreds.activeDirectoryEndpointUrl `
-        -GraphResourceId $RawCreds.activeDirectoryGraphResourceId | out-null;
+        -GraphEndpoint $RawCreds.activeDirectoryGraphResourceId | out-null;
 
     $servicePrincipalKey = ConvertTo-SecureString $RawCreds.clientSecret.replace("'", "''") -AsPlainText -Force
     $psCredential = New-Object System.Management.Automation.PSCredential($RawCreds.clientId, $servicePrincipalKey)
 
     Connect-AzAccount -ServicePrincipal -Tenant $Env:tenantId -Credential $psCredential -Environment $Env:azureCloud | out-null;
-
-    Set-AzContext -SubscriptionId $RawCreds:subscriptionId -TenantId $Env:tenantId | out-null;
+    Set-AzContext -SubscriptionId $RawCreds.subscriptionId -TenantId $RawCreds.tenantId | out-null;
 }
 
 if (-NOT $Env:azureCloud -eq "Prod") {
