ca602ff640c97f505a38618729dca3dff88d7d97
Eran Toledano <erant10@gmail.com>
2021-04-26 14:50:53 -0700

    Sentinel Content Deployment Script

---
+++ .github/workflows/azure-sentinel-deploy.ps1
@@ -0,0 +1,30 @@
+function ConnectAzCloud {
+    $RawCreds = $Env:creds | ConvertFrom-Json
+
+    Clear-AzContext -Scope Process;
+    Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue;
+    
+    Add-AzEnvironment `
+        -Name "CustomEnvironment" `
+        -ActiveDirectoryServiceEndpointResourceId $RawCreds.activeDirectoryServiceEndpointResourceId `
+        -ActiveDirectoryEndpoint $RawCreds.activeDirectoryEndpointUrl `
+        -ResourceManagerEndpoint $RawCreds.resourceManagerEndpointUrl `
+        -GraphEndpoint $RawCreds.graphEndpoint | out-null;
+
+    $servicePrincipalKey = ConvertTo-SecureString $RawCreds.clientSecret.replace("'", "''") -AsPlainText -Force
+    $psCredential = New-Object System.Management.Automation.PSCredential($RawCreds.clientId, $servicePrincipalKey)
+
+    Connect-AzAccount -ServicePrincipal -Tenant $RawCreds.tenantId -Credential $psCredential -Environment "CustomEnvironment" | out-null;
+    Set-AzContext -SubscriptionId $RawCreds.subscriptionId -TenantId $RawCreds.tenantId | out-null;
+}
+
+if ($Env:useDefaultCloud -eq 'false') {
+    Write-Output "Attempting Sign In to Azure Cloud"
+    ConnectAzCloud
+}
+
+Write-Output "Starting Deployment for Files in path: $Env:directory"
+Get-ChildItem $Env:directory -Filter *.json |
+ForEach-Object {
+    New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $_.FullName -logAnalyticsWorkspaceName $Env:workspaceName
+}
