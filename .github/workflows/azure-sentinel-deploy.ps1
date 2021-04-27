5d9c3a35bb3fab4d76bd9089ee85be82e7f705b7
Eran Toledano <erant10@gmail.com>
2021-04-26 17:18:35 -0700

    Sentinel Content Deployment Script

--- .github/workflows/azure-sentinel-deploy.ps1
+++ .github/workflows/azure-sentinel-deploy.ps1
@@ -15,7 +15,7 @@ function ConnectAzCloud {
     $psCredential = New-Object System.Management.Automation.PSCredential($RawCreds.clientId, $servicePrincipalKey)
 
     Connect-AzAccount -ServicePrincipal -Tenant $RawCreds.tenantId -Credential $psCredential -Environment "CustomEnvironment" | out-null;
-    Set-AzContext -SubscriptionId $RawCreds.subscriptionId -TenantId $RawCreds.tenantId | out-null;
+    Set-AzContext -Tenant $RawCreds.tenantId | out-null;
 }
 
 if ($Env:useDefaultCloud -eq 'false') {
