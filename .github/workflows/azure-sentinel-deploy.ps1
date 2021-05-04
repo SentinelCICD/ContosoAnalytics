677909a8e5d7d5df0d28c9cf2d1e6776543526d1
Eran Toledano <erant10@gmail.com>
2021-05-04 14:43:18 -0700

    Sentinel Content Deployment Script

--- .github/workflows/azure-sentinel-deploy.ps1
+++ .github/workflows/azure-sentinel-deploy.ps1
@@ -5,20 +5,20 @@ function ConnectAzCloud {
     Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue;
     
     Add-AzEnvironment `
-        -Name "CustomEnvironment" `
-        -ActiveDirectoryServiceEndpointResourceId $RawCreds.activeDirectoryServiceEndpointResourceId `
+        -Name $Env:cloudEnv `
         -ActiveDirectoryEndpoint $RawCreds.activeDirectoryEndpointUrl `
         -ResourceManagerEndpoint $RawCreds.resourceManagerEndpointUrl `
-        -GraphEndpoint $RawCreds.graphEndpoint | out-null;
+        -ActiveDirectoryServiceEndpointResourceId $RawCreds.activeDirectoryServiceEndpointResourceId `
+        -GraphEndpoint $RawCreds.graphEndpointUrl | out-null;
 
     $servicePrincipalKey = ConvertTo-SecureString $RawCreds.clientSecret.replace("'", "''") -AsPlainText -Force
     $psCredential = New-Object System.Management.Automation.PSCredential($RawCreds.clientId, $servicePrincipalKey)
 
-    Connect-AzAccount -ServicePrincipal -Tenant $RawCreds.tenantId -Credential $psCredential -Environment "CustomEnvironment" | out-null;
+    Connect-AzAccount -ServicePrincipal -Tenant $RawCreds.tenantId -Credential $psCredential -Environment $Env:cloudEnv | out-null;
     Set-AzContext -Tenant $RawCreds.tenantId | out-null;
 }
 
-if ($Env:useDefaultCloud -eq 'false') {
+if ($Env:cloudEnv -ne 'AzureCloud') {
     Write-Output "Attempting Sign In to Azure Cloud"
     ConnectAzCloud
 }
